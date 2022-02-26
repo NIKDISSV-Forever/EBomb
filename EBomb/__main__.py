3ad83647a70cd4364eb78d06b54699fc5e7df912
1i2i <m3u8q@bk.ru>
2022-02-26 14:13:52 +0600

    __main__ fix

--- EBomb/__main__.py
+++ EBomb/__main__.py
@@ -5,14 +5,12 @@ import EBomb
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--targets', '-t', type=str, nargs='+')
+    parser.add_argument('--targets', '-t', type=str, nargs='+', default=[])
     parser.add_argument('--threads-count', '-tc', type=int)
     parser.add_argument('--proxy', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--forever', '-f', action=argparse.BooleanOptionalAction, default=True)
-    parser.add_argument('--verbose', '-v', action=argparse.BooleanOptionalAction, default=True)
-    args = parser.parse_args()
-    print(args)
-    EBomb.EBomb(**args.__dict__)
+    parser.add_argument('--forever', action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument('--verbose', action=argparse.BooleanOptionalAction, default=True)
+    EBomb.EBomb(**parser.parse_args().__dict__)
 
 
 if __name__ == '__main__':
